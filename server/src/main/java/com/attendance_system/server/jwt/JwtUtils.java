f086ad6d48cedec8c59457ff210890673b4902a4
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-02-12 23:22:19 +0530

    Initial commit - Attendance system

---
+++ server/src/main/java/com/attendance_system/server/jwt/JwtUtils.java
@@ -0,0 +1,9 @@
+package com.attendance_system.server.jwt;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+    private static final logger = LoggerFactory.getLogger(JwtUtils.class);
+}
